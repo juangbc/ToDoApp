a573b67749dc21b152119175b641fbbd97a94a8b
DESKTOP-E2UMV8S\Juan Garcia-Berdoy <juan.garcia-berdoy@thesisdigital.de>
2018-10-02 16:26:08 +0200

    major changes: changes in dashboard template to contain only one main node that then will be cloned, major changes to js file creating necessary objects and functions that iterate over these, minor changes to stylessheet

--- startpage/dashboard.page/toDoAppDashboard.js
+++ startpage/dashboard.page/toDoAppDashboard.js
@@ -3,121 +3,100 @@
 
 window.onload = main();
 
+
 function main() {
-    var userCategoriesArray = [
-        ["Persönliches", "Hunderunde", "Mich einkaufen"],
-        ["Arbeit", "HalloWelt", "Präsentation"]
-    ];
-    createCategories(userCategoriesArray);
+    var categoryObject = new Category("category1","Foo bar",["Dings", "Das", "Dengs","Dongs"]);
+    var categoryObject1 = new Category("category2","Noch ein Titel",["Arbeit", "Struktur"]);
+    var arrayOfObjects = [categoryObject,categoryObject1];
+    saveCategories(arrayOfObjects);
+    cloneNecessaryColumnNodes(arrayOfObjects);
+    cloneNecessaryTaskNodes(arrayOfObjects);
+    setAllTasks(arrayOfObjects);
 }
 
-function createElement(varType,className){
-    var x = document.createElement(varType);
-    x.className = className;
+function styleDiv() {
+    document.getElementById("outer-taskfield-label").style.backgroundColor = "red";
+    document.body.style.backgroundColor = "#6BB3EC";
+    document.getElementsByClassName("task-name")[0].innerHTML = "Milk";
+}
+
+function Category(key,categoryTitle, taskName) {
+    this.key = key;
+    this.categoryTitle = categoryTitle;
+    this.taskName = taskName;
+     for (var i = 0; i< taskName.length; i++) {
+     this.taskName[i] = taskName[i];
+     }
+}
+
+function saveCategories (arrayOfObjects) {
+    var x = arrayOfObjects.length-1;
+    for (var i = 0; i < x; i++) {
+        saveCategoriesIntoLocalStorage(arrayOfObjects[i].key,arrayOfObjects[i]);
+    }
+}
+
+function saveCategoriesIntoLocalStorage (key, categoryObject) {
+    localStorage.setItem(key, JSON.stringify(categoryObject));
+}
+
+function readCategoriesFromLocalStorage(key) {
+    var x = JSON.parse(localStorage.getItem(key));
     return x;
 }
 
-
-function createColumn() {
-    return createElement("div","column");
+function setTitleOfCategoryInDom(key) {
+    var categoryTitle = readCategoriesFromLocalStorage(key).categoryTitle;
+    document.getElementsByClassName("category-title")[0].innerHTML = categoryTitle;
 }
 
-function createCheckIcon() {
-    var checkIconContainer = createElement("span", "checkmark");
-    var checkIcon = createElement("i","material-icons check-icon");
-    var checkIconTag = document.createTextNode("check");
-    checkIcon.appendChild(checkIconTag);
-    checkIconContainer.appendChild(checkIcon);
-    return checkIconContainer;
+function setTaskNameInDom(key, index) {
+    var taskName = readCategoriesFromLocalStorage(key).taskName[index];
+    document.getElementsByClassName("task-name")[index].innerHTML = taskName;
 }
 
-function createInnerCheckbox() {
-    var checkbox = createElement("input","checkbox");
-    checkbox.type = "checkbox";
-    return checkbox;
-}
-
-function createFooter() {
-    var footerButton = createElement("button","footer-button");
-    var textButton = document.createTextNode("+ NEUE AUFGABE");
-    footerButton.appendChild(textButton);
-
-    var divFooter = createElement("div","footer-category");
-    divFooter.appendChild(footerButton);
-    return divFooter;
-}
-
-function createTaskFromArray(array) {
-    var taskContainer = createElement("div","task");
-    var label = createElement("label","task-checkbox-label");
-    label.appendChild(createInnerCheckbox());
-    label.appendChild(createCheckIcon());
-
-
-    var spanName = createElement("span","task-name");
-    spanName.appendChild(document.createTextNode(array));
-    label.appendChild(spanName);
-
-    var binIcon = createElement("i","material-icons bin");
-    var text = document.createTextNode("delete");
-    binIcon.appendChild(text);
-
-    var binContainer = createElement("div","bin-icon-container");
-    binContainer.appendChild(binIcon);
-    label.appendChild(binContainer);
-    taskContainer.appendChild(label);
-    return taskContainer;
-}
-
-function createUpperPartColumnFromArray(array) {
-    var categoryHeader = createElement("h2","category-title");
-    categoryHeader.appendChild(document.createTextNode(array));
-
-    var upperPartColumn = createElement("div","column-upper-part");
-    upperPartColumn.appendChild(categoryHeader);
-    return upperPartColumn;
-}
-
-function createOuterLabel() {
-    var outerLabel = createElement("label","outer-taskfield-label outer-label-");
-
-    var outerCheckbox = createElement("input","checkbox");
-    outerCheckbox.type = "checkbox";
-    outerLabel.appendChild(outerCheckbox);
-    return outerLabel
-}
-
-function createDefaultColumn() {
-    var divDefault = createElement("div","column column-default");
-
-    var titleDefaultColumn = createElement("h2","defaultColumnTitleText");
-    var textDefaultColumn = document.createTextNode("+ Neue Kategorie");
-    titleDefaultColumn.appendChild(textDefaultColumn);
-    divDefault.appendChild(titleDefaultColumn);
-    return divDefault;
-}
-
-function createCategories(array) {
-    for (var j = 0; j < array.length; j++) {
-        var divcategory = createColumn();
-        var upperPartColumn = createUpperPartColumnFromArray(array[j][0]);
-        divcategory.appendChild(upperPartColumn);
-
-        for (var i = 1; i < array[j].length; i++) {
-            var outerLabel = createOuterLabel();
-            outerLabel.appendChild(createTaskFromArray(array[j][i]));
-            upperPartColumn.appendChild(outerLabel);
-        }
-
-        divcategory.appendChild(createFooter());
-        document.getElementById("container").appendChild(divcategory);
+function setTaskNames(key) {
+    var x = readCategoriesFromLocalStorage(key).taskName.length;
+    for (var i = 0; i < x; i++) {
+        setTaskNameInDom(key,i);
     }
-    document.getElementById("container").appendChild(createDefaultColumn());
 }
 
+function setAllTasks(arrayOfObjects) {
+    var x = arrayOfObjects.length;
+    for (var i = 0; i < x; i++) {
+        setTaskNames(arrayOfObjects[i].key);
+    }
+}
+/*
+function cloneNecessaryTaskNodes(key, columnIndex) {
+    var x = readCategoriesFromLocalStorage(key).taskName.length;
+    console.log(x);
+    for (var i = 1; i < x; i++) {
+        var itm = document.getElementsByClassName("outer-taskfield-label")[0];
+        var cln = itm.cloneNode(true);
+        document.getElementsByClassName("column-upper-part")[columnIndex].appendChild(cln);
+    }
+}
+*/
 
+function cloneNecessaryTaskNodes(arrayOfObjects) {
+    var x = arrayOfObjects.length;
+    for (var j = 0; j < x; j++) {
+        var y = readCategoriesFromLocalStorage(arrayOfObjects[j].key).taskName.length;
+        for (var i = 1; i < y; i++) {
+            var itm = document.getElementsByClassName("outer-taskfield-label")[0];
+            var cln = itm.cloneNode(true);
+            document.getElementsByClassName("column-upper-part")[j].appendChild(cln);
+        }
+    }
+}
 
-
-
-document.getElementById("myBtn").addEventListener("click", displayDate);
-
+function cloneNecessaryColumnNodes(arrayOfObjects) {
+    var x = arrayOfObjects.length;
+    for (var i = 1; i < x; i++) {
+        var itm = document.getElementsByClassName("column")[0];
+        var cln = itm.cloneNode(true);
+        document.getElementsByClassName("flex-container")[0].appendChild(cln);
+    }
+}
