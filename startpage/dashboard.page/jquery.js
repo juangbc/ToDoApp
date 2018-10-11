545a88a1502f02d3505a061ff4c4365ae7efa22e
DESKTOP-E2UMV8S\Juan Garcia-Berdoy <juan.garcia-berdoy@thesisdigital.de>
2018-10-11 15:55:15 +0200

    major changes to jquery task select logic and minor refactoring

--- startpage/dashboard.page/jquery.js
+++ startpage/dashboard.page/jquery.js
@@ -2,12 +2,27 @@
  * Created by Juan Garcia-Berdoy on 09.10.2018.
  */
 
-$(".task-container").click(function() {
- //   var myClass = ;
-    if (!$(this).hasClass("selected")) {
-        $(this).addClass("selected");
+$(".task-container").on('click', function() {
+    if (!$(this).hasClass("selected-task")) {
+        $(this).addClass("selected-task");
+        $(this).siblings(".task-container").addClass("sibling-of-selected-task");
     }
+
     else {
-        $(this).removeClass("selected");
+        $(this).removeClass("selected-task");
+
+        if(!$(this).siblings().hasClass("selected-task")) {
+            $(this).siblings().removeClass("sibling-of-selected-task");
+        }
+        else {
+        $(this).addClass("sibling-of-selected-task");
+        }
     }
 });
+
+$(".task-container").on('click', function() {
+    if ($(".selected-task").hasClass("sibling-of-selected-task")) {
+        $(".selected-task").removeClass("sibling-of-selected-task");
+    }
+});
+
