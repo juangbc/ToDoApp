7180fb50aa3803d31e0465967da33c19249c6300
DESKTOP-E2UMV8S\Juan Garcia-Berdoy <juan.garcia-berdoy@thesisdigital.de>
2018-10-12 10:11:55 +0200

    refactoring: rename files for better readability and delete useless testfiles in package

--- startpage/dashboard.page/dashboardPage.js
+++ startpage/dashboard.page/dashboardPage.js
@@ -28,8 +28,7 @@ function Category(key,categoryTitle, taskName) {
 }
 
 function saveCategories (arrayOfObjects) {
-    var x = arrayOfObjects.length;
-    for (var i = 0; i < x; i++) {
+    for (var i = 0; i < arrayOfObjects.length; i++) {
         saveCategoriesIntoLocalStorage(arrayOfObjects[i].key,arrayOfObjects[i]);
     }
 }
@@ -49,8 +48,7 @@ function setTitleOfCategoryInDom(categoryKey,categoryIndex) {
 }
 
 function setAllCategoryTitles(categoriesArray) {
-    var numberOfCategories = categoriesArray.length;
-    for (var i = 0; i < numberOfCategories; i++) {
+    for (var i = 0; i < categoriesArray.length; i++) {
         setTitleOfCategoryInDom(categoriesArray[i].key,i);
     }
 }
@@ -72,10 +70,10 @@ function setAllTaskNames(categoriesArray) {
     }
 }
 
-function cloneNecessaryTaskNodes(arrayOfObjects) {
-    var x = arrayOfObjects.length;
-    for (var j = 0; j < x; j++) {
-        var y = readCategoriesFromLocalStorage(arrayOfObjects[j].key).taskName.length;
+function cloneNecessaryTaskNodes(categoriesArray) {
+    var numberOfCategories = categoriesArray.length;
+    for (var j = 0; j < numberOfCategories; j++) {
+        var y = readCategoriesFromLocalStorage(categoriesArray[j].key).taskName.length;
         for (var i = 1; i < y; i++) {
             var originalTaskNode = document.getElementsByClassName("task-container")[0];
             var clonedTaskNode = originalTaskNode.cloneNode(true);
@@ -84,43 +82,10 @@ function cloneNecessaryTaskNodes(arrayOfObjects) {
     }
 }
 
-function cloneNecessaryColumnNodes(arrayOfObjects) {
-    var x = arrayOfObjects.length;
-    for (var i = 0; i < x; i++) {
+function cloneNecessaryColumnNodes(categoriesArray) {
+    for (var i = 0; i < categoriesArray.length; i++) {
         var originalColumnNode = document.getElementsByClassName("column")[0];
         var clonedColumnNode = originalColumnNode.cloneNode(true);
         document.getElementsByClassName("flex-container")[0].appendChild(clonedColumnNode);
     }
 }
-
-/*
-function functionTest() {
-    if (!document.getElementsByClassName("task-container")[4].classList.contains("selected")) {
-        document.getElementsByClassName("task-container")[4].classList.add("selected");
-        document.getElementsByClassName("task-container")[4].style.marginLeft = "-2.5em";
-        document.getElementsByClassName("task-container")[4].style.marginBottom = "1em";
-        document.getElementsByClassName("task-container")[4].style.width = "27em";
-        document.getElementsByClassName("task-container")[4].style.paddingLeft = "2.5em";
-        document.getElementsByClassName("task-container")[4].style.paddingTop = "1em";
-        document.getElementsByClassName("task-container")[4].style.paddingBottom = "1em";
-        document.getElementsByClassName("task-container")[4].style.background =
-            '-webkit-linear-gradient(45deg, #6BB3EC 0%, #6ED4E3 100%)';
-       var x =  document.getElementsByClassName("task-container")[4].className;
-        console.log(x);
-
-       // SOMETHING WITH THIS
-    }
-    else
-    {
-        document.getElementsByClassName("task-container")[4].classList.remove("selected");
-     //   document.getElementById("theMac").style.all = "unset";
-        document.getElementsByClassName("task-container")[4].style.margin = "0";
-        document.getElementsByClassName("task-container")[4].style.width = "100%";
-        document.getElementsByClassName("task-container")[4].style.padding = "0";
-      //  document.getElementsByClassName("task-container")[4].style.background = "red";
-
-    }
-}
-
-*/
-
